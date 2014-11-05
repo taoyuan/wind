b11cf3b4ebacb2cab95d04a725505dc4643c4201
taoyuan <torworx@gmail.com>
2014-11-05 10:30:23 +0800

    added intercept for logger

---
+++ test/logger.test.js
@@ -0,0 +1,83 @@
+var assert = require('chai').assert;
+var Logger = require('../lib/wide/logger').Logger;
+
+describe('logger', function () {
+
+    var logger;
+
+    beforeEach(function () {
+        logger = new Logger({
+            emitErrs: true
+        });
+        logger.on('error', function (err) {
+            throw err;
+        })
+    });
+
+    describe('.intercept', function () {
+        it('should add the function and call it when a log occurs', function (next) {
+            var called;
+            var data = {
+                'some': 'thing'
+            };
+
+            logger.intercept(function (log) {
+                called = true;
+
+                assert.deepEqual(log, {
+                    level: 'warn',
+                    id: 'foo',
+                    message: 'bar',
+                    data: data
+                });
+                assert.equal(log.data, data);
+            });
+
+            logger.log('warn', 'foo', 'bar', data);
+
+            assert.isTrue(called);
+            next();
+        });
+
+        it('should call the interceptors by order before emitting the event', function (next) {
+            var called = [];
+
+            logger.intercept(function () {
+                called.push(1);
+            });
+            logger.intercept(function () {
+                called.push(2);
+            });
+
+            logger.log('warn', 'foo', 'bar');
+
+            assert.deepEqual(called, [1, 2]);
+            next();
+        });
+
+        //it('should call the interceptors along the chain', function (next) {
+        //    var called = [];
+        //    var childLogger = logger.geminate();
+        //
+        //    childLogger.intercept(function () {
+        //        called.push(1);
+        //    });
+        //    logger.intercept(function () {
+        //        called.push(3);
+        //    });
+        //
+        //    childLogger.on('log', function () {
+        //        called.push(2);
+        //    });
+        //    logger.on('log', function () {
+        //        called.push(4);
+        //    });
+        //
+        //    childLogger.log('warn', 'foo', 'bar');
+        //
+        //    assert.deepEqual(called, [1, 2, 3, 4]);
+        //    next();
+        //});
+    });
+
+});
